674e7acf918a0c17eb71a39bec78c0add3fcd64c
north-ross <northharperross@gmail.com>
2022-11-29 13:26:53 -0800

    points update python

--- BigfootMap/PythonServer/UpdatePoints.bat
+++ BigfootMap/PythonServer/UpdatePoints.bat
@@ -1,2 +1,4 @@
+call "C:\Users\North\anaconda3\Scripts\activate.bat"
+activate lidarenv
 python "D:\Documents\NorthsWebProjects\north-ross.github.io\BigfootMap\PythonServer\UpdatePointsJSON.py"
-pause
+cmd /k
