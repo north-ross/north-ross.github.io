024d20050a40952cf9b9d1469381b27a319d0ba2
north-ross <northharperross@gmail.com>
2022-12-01 21:19:53 -0800

    Default dark mode
    Changed default base map to stadia dark for preformace + aesthetics

--- BigfootMap/PythonServer/UpdatePoints0.bat
+++
@@ -1,4 +0,0 @@
-call "C:\Users\North\anaconda3\Scripts\activate.bat"
-activate lidarenv
-python "D:\Documents\NorthsWebProjects\north-ross.github.io\BigfootMap\PythonServer\UpdatePointsJSON.py"
-cmd /k
